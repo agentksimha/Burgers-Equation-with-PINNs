510e4fe30adffbcca7e937bea5f63cabeb1b87c0
agentksimha <krishna2005simha@gmail.com>
2025-10-08 13:09:41 +0530

    Update physics_loss.py

--- physics_loss.py
+++ physics_loss.py
@@ -7,7 +7,7 @@ def pde_loss(model,x,t,alpha=0.01):
     u_t = torch.autograd.grad(u,t,grad_outputs=torch.ones_like(u),create_graph=True)[0]
     u_x = torch.autograd.grad(u,x,grad_outputs=torch.ones_like(u),create_graph=True)[0]
     u_xx = torch.autograd.grad(u_x,x,grad_outputs=torch.ones_like(u_x) , create_graph=True)[0]
-    loss_pde = torch.mean((u_t - alpha * u_xx)**2)
+    loss_pde = torch.mean((u_t - alpha * u_xx)**2) #for burgers equation we can simply change this to u_t - u*u_x - V * u_xx where V is a constant
     return loss_pde
 
 def total_loss(model, X_f, T_f, X_bc, T_bc, U_bc, X_ic, T_ic, U_ic, alpha=0.01):
